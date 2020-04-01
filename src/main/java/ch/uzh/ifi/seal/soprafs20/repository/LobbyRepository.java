e060183df9ca647948000dcfe6b7d589930c09e1
JaanWilli <jan.willi@uzh.ch>
2020-04-01 20:23:38 +0200

    Small changes to the Repository classes to support registration/lobby creation

--- src/main/java/ch/uzh/ifi/seal/soprafs20/repository/LobbyRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/repository/LobbyRepository.java
@@ -4,11 +4,12 @@ import ch.uzh.ifi.seal.soprafs20.entity.Lobby;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository("lobbyRepository")
 public interface LobbyRepository extends JpaRepository<Lobby, Long> {
 
-    Lobby findByName(String name);
+    List<Lobby> findByName(String name);
+    List<Lobby> findByHost(String host);
     Lobby findByLobbyId(Long id);
 }
--- src/main/java/ch/uzh/ifi/seal/soprafs20/repository/PlayerRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/repository/PlayerRepository.java
@@ -4,9 +4,14 @@ import ch.uzh.ifi.seal.soprafs20.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("playerRepository")
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     Player findByUsername(String username);
     Player findByUsernameAndLobbyId(String username, Long lobbyId);
+    Player findByAuthToken(String authToken);
+    Player findByToken(String token);
+    List<Player> findByLobbyId(Long lobbyId);
 }
