f575108f029317f381a61a9ff62f0ac4807e5201
Davfon <davide.fontanella@uzh.ch>
2020-04-02 12:35:33 +0200

    Created Mock Classes for Card & Hand to ensure build does not fail.

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Card.java
@@ -0,0 +1,9 @@
+package ch.uzh.ifi.seal.soprafs20.entity;
+
+public class Card {
+    //private attributes
+
+    //constructor?
+
+    //methods
+}
---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Hand.java
@@ -0,0 +1,12 @@
+package ch.uzh.ifi.seal.soprafs20.entity;
+
+public class Hand {
+    //private attributes
+
+    public Card pop(int index) {
+        return new Card();
+    }
+
+    public void push(Card card) {
+    }
+}
