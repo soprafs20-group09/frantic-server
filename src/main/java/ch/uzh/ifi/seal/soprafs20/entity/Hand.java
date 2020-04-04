0607c78f77a9dce64503f467010195f4daaf88bb
sei-nah <sina.ktumhard@uzh.ch>
2020-04-04 16:36:44 +0200

    Hand: Updated push(), pop(index), added size()

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Hand.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Hand.java
@@ -1,12 +1,20 @@
 package ch.uzh.ifi.seal.soprafs20.entity;
 
+import java.util.List;
+
 public class Hand {
     //private attributes
+    private List<Card> cards;
 
     public Card pop(int index) {
-        return new Card();
+        return cards.remove(index);
     }
 
     public void push(Card card) {
+        cards.add(card);
+    }
+
+    public int size(){
+        return cards.size();
     }
 }
