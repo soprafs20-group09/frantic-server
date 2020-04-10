e0bcc1b435d5c4c6c5032a0ef32a3a97cf5a67e3
sei-nah <sina.ktumhard@uzh.ch>
2020-04-10 18:54:22 +0200

    PileTest: Updated tests to fit new drawStack constructor

--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/PileTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/PileTest.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.soprafs20.entity;
 
 import ch.uzh.ifi.seal.soprafs20.constant.Color;;
+import ch.uzh.ifi.seal.soprafs20.constant.Type;
 import ch.uzh.ifi.seal.soprafs20.entity.cards.NumberCard;
 import org.junit.jupiter.api.Test;
 
@@ -20,66 +21,43 @@ class PileTest {
         assertEquals(0, drawStack.size());
     }
 
-   /* @Test
-    public void createStack_getStackWithElements() {
-        NumberCard blue1 = new NumberCard(Color.BLUE, 1);
-        NumberCard red4 = new NumberCard(Color.RED, 4);
-        Card[] cards = {blue1, red4};
-
-        DrawStack drawStack = new DrawStack(cards);
-
-        assertEquals(2, drawStack.size());
-    }*/
-
     @Test
     public void pushCardsintoStack() {
         NumberCard blue1 = new NumberCard(Color.BLUE, 1, 1);
 
         DrawStack drawStack = new DrawStack();
         drawStack.push(blue1);
-
-        assertEquals(1, drawStack.size());
+        Card peeked = drawStack.peek();
+        assertEquals(Color.BLUE, peeked.getColor());
     }
 
     @Test
     public void peek1stCard() {
-        NumberCard blue1 = new NumberCard(Color.BLUE, 1, 1);
-
         DrawStack drawStack = new DrawStack();
-        drawStack.push(blue1);
 
         Card peeked = drawStack.peek();
 
-        assertEquals(Color.BLUE, peeked.getColor());
+        assertEquals(Type.NUMBER, peeked.getType());
     }
 
-    /*@Test
+    // This works, has been tested in earlier version
+    @Test
     public void peek2ndCard() {
-        NumberCard blue1 = new NumberCard(Color.BLUE, 1);
-        NumberCard red4 = new NumberCard(Color.RED, 4);
-        Card[] cards = {blue1, red4};
-
-        DrawStack drawStack = new DrawStack(cards);
+        DrawStack drawStack = new DrawStack();
 
         Card peeked = drawStack.peekSecond();
-        assertEquals(Color.BLUE, peeked.getColor());
+        assertEquals(Type.NUMBER, peeked.getType());
 
-    }*/
+    }
 
-    /*@Test
+    @Test
     public void popCard() {
-        NumberCard blue1 = new NumberCard(Color.BLUE, 1,1);
-        NumberCard red4 = new NumberCard(Color.RED, 4, 2);
-        Card[] cards = {blue1, red4};
-
-        DrawStack drawStack = new DrawStack(cards);
-
-        assertEquals(2, drawStack.size());
-
+        DrawStack drawStack = new DrawStack();
+        int oldSize = drawStack.size();
         Card popped = drawStack.pop();
 
-        assertEquals(Color.RED, popped.getColor());
-        assertEquals(1, drawStack.size());
+        assertEquals(Type.NUMBER, popped.getType());
+        assertEquals(oldSize-1, drawStack.size());
 
-    }*/
+    }
 }
