d0f45680a72f8b52515692ede418fc188c890f0e
sei-nah <sina.krumhard@uzh.ch>
2020-04-16 15:46:38 +0200

    ExchangeAction: Added Exchange functionality. Tests pass

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/actions/ExchangeAction.java
@@ -0,0 +1,83 @@
+package ch.uzh.ifi.seal.soprafs20.entity.actions;
+
+import ch.uzh.ifi.seal.soprafs20.constant.Value;
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ExchangeAction implements Action {
+
+    private Player initiator;
+    private Player target;
+    private int[] exchangeCards; // cards the initiator gives to the target
+
+
+    public ExchangeAction(Player initiator, Player target, int[] cards) {
+        this.initiator = initiator;
+        this.target = target;
+        if (cards.length > 1 && cards[0] < cards[1]) {
+            int temp = cards[0];
+            cards[0] = cards[1];
+            cards[1] = temp;
+        }
+        this.exchangeCards = cards;
+    }
+
+    public Player getInitiator() {
+        return this.initiator;
+    }
+
+    public Player[] getTargets() {
+        return new Player[]{this.target};
+    }
+
+    public boolean isCounterable() {
+        return true;
+    }
+
+    public void perform() {
+        ArrayList<Card> initiatorCards = new ArrayList<Card>();
+        for (int i : exchangeCards) {
+            Card toPush = this.initiator.peekCard(i);
+            if (toPush.getValue() != Value.FUCKYOU) {
+                toPush = this.initiator.popCard(i);
+                initiatorCards.add(toPush);
+            }
+        }
+
+        if (this.target.getHandSize() < 2) {
+            Card toInitiator = this.target.popCard(0);
+            this.initiator.pushCardToHand(toInitiator);
+        }
+        else {
+            // gernerate random cards to be pushed to the initiator
+            Random r = new Random();
+            int random1 = r.nextInt(target.getHandSize());
+            int random2 = r.nextInt(target.getHandSize());
+            while (random1 == random2) {
+                random2 = r.nextInt(target.getHandSize());
+            }
+
+            // remove card with bigger index first
+            if (random1 > random2) {
+                Card toInitiator = this.target.popCard(random1);
+                this.initiator.pushCardToHand(toInitiator);
+                toInitiator = this.target.popCard(random2);
+                this.initiator.pushCardToHand(toInitiator);
+            }
+            else {
+                Card toInitiator = this.target.popCard(random2);
+                this.initiator.pushCardToHand(toInitiator);
+                toInitiator = this.target.popCard(random1);
+                this.initiator.pushCardToHand(toInitiator);
+            }
+        }
+
+        for (Card c : initiatorCards) {
+            this.target.pushCardToHand(c);
+        }
+
+    }
+}
---
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/actions/ExchangeActionTest.java
@@ -0,0 +1,69 @@
+package ch.uzh.ifi.seal.soprafs20.entity.actions;
+
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExchangeActionTest {
+
+    @Test
+    public void exchange_bothHaveTwoCards() {
+        Player initiator = new Player();
+        Player target = new Player();
+        Card blue1 = new Card(Color.BLUE, 1, 1);
+        Card green2 = new Card(Color.GREEN, 2, 2);
+        Card red3 = new Card(Color.RED, 3, 3);
+        Card yellow4 = new Card(Color.YELLOW, 4, 4);
+        initiator.pushCardToHand(blue1);
+        initiator.pushCardToHand(green2);
+        target.pushCardToHand(red3);
+        target.pushCardToHand(yellow4);
+
+
+        ExchangeAction exchange = new ExchangeAction(initiator, target, new int[]{0, 1});
+        exchange.perform();
+
+        // dummy test... tested with debugger. works
+        assertEquals(2, initiator.getHandSize());
+    }
+
+    @Test
+    public void targetHasOneCard() {
+        Player initiator = new Player();
+        Player target = new Player();
+        Card blue1 = new Card(Color.BLUE, 1, 1);
+        Card green2 = new Card(Color.GREEN, 2, 2);
+        Card red3 = new Card(Color.RED, 3, 3);
+        initiator.pushCardToHand(blue1);
+        initiator.pushCardToHand(green2);
+        target.pushCardToHand(red3);
+
+        ExchangeAction exchange = new ExchangeAction(initiator, target, new int[]{0, 1});
+        exchange.perform();
+
+        assertEquals(2, target.getHandSize());
+        assertEquals(1, initiator.getHandSize());
+    }
+
+    @Test
+    public void initiatorHasOneCard() {
+        Player initiator = new Player();
+        Player target = new Player();
+        Card blue1 = new Card(Color.BLUE, 1, 1);
+        Card red3 = new Card(Color.RED, 3, 3);
+        Card yellow4 = new Card(Color.YELLOW, 4, 4);
+        initiator.pushCardToHand(blue1);
+        target.pushCardToHand(red3);
+        target.pushCardToHand(yellow4);
+
+        ExchangeAction exchange = new ExchangeAction(initiator, target, new int[]{0});
+        exchange.perform();
+
+        assertEquals(2, initiator.getHandSize());
+        assertEquals(1, target.getHandSize());
+    }
+
+}
