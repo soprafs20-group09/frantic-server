b68e32d17a125a45f1e1d055dc44fc318b45fc43
Davfon <davide.fontanella@uzh.ch>
2020-04-21 18:00:54 +0200

    TimeBomb: now contains GameRound instead of Game

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/TimeBombEvent.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/TimeBombEvent.java
@@ -3,23 +3,25 @@ package ch.uzh.ifi.seal.soprafs20.entity.events;
 import ch.uzh.ifi.seal.soprafs20.constant.Color;
 import ch.uzh.ifi.seal.soprafs20.constant.Value;
 import ch.uzh.ifi.seal.soprafs20.entity.DiscardPile;
-import ch.uzh.ifi.seal.soprafs20.entity.Game;
+import ch.uzh.ifi.seal.soprafs20.entity.GameRound;
 import ch.uzh.ifi.seal.soprafs20.entity.Player;
 
 import java.util.List;
 
 public class TimeBombEvent implements Event {
-    private Game game;
+    private GameRound gameRound;
+
+    public TimeBombEvent(GameRound gameRound){
+        this.gameRound = gameRound;
+    }
 
     public String getName(){
         return "time-bomb";
     };
+
     public void performEvent() {
-        game.getCurrentGameRound().setTimeBomb(true);
-        List<Player> players = game.getCurrentGameRound().getListOfPlayers();
-        for (Player player : players) {
-            game.getCurrentGameRound().initMap(player, 0);
-        }
+        gameRound.setTimeBomb(true);
     };
+
     public String getMessage() {return "Tick ... Tick ... Tick ... Boom! Everyone has three turns left! Defuse the Bomb and earn a reward by winning the round or let the Bomb explode and everyone's points in this round get doubled!";};
 }
