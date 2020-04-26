a426b860e5c4ff8083f9af1600e01d61e2e51c23
regloff <45421260+regloff@users.noreply.github.com>
2020-04-26 16:08:41 +0200

    NiceTryActionTest, CounterAttackActionTest: Added test

---
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/actions/CounterAttackActionTest.java
@@ -0,0 +1,38 @@
+package ch.uzh.ifi.seal.soprafs20.entity.actions;
+
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.constant.Type;
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.Chat;
+import ch.uzh.ifi.seal.soprafs20.entity.DiscardPile;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CounterAttackActionTest {
+
+    @Test
+    public void wishColor() {
+        DiscardPile discardPile = new DiscardPile();
+        Player initiator = new Player();
+        initiator.setUsername("Testplayer");
+
+        CounterAttackAction counterattack = new CounterAttackAction(initiator, Color.BLUE, discardPile);
+        List<Chat> chat = counterattack.perform();
+
+        String chatMessage = chat.get(0).getMessage();
+        String chatType = chat.get(0).getType();
+        String chatIcon = chat.get(0).getIcon();
+        Card wished = discardPile.peek();
+
+        assertEquals(Color.BLUE, wished.getColor());
+        assertEquals(Type.WISH, wished.getType());
+        assertEquals("Testplayer wished blue.", chatMessage);
+        assertEquals("event", chatType);
+        assertEquals("special:counterattack", chatIcon);
+    }
+
+}
