ce4c0be233e7f8d002179fd33b6be54e89517a5c
JaanWilli <jan.willi@uzh.ch>
2020-04-28 11:50:57 +0200

    MexicanStandoffEvent: added event logic and test

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/MexicanStandoffEvent.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/MexicanStandoffEvent.java
@@ -1,11 +1,38 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.Pile;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+
+import java.util.List;
+
 public class MexicanStandoffEvent implements Event {
+
+    private final List<Player> listOfPlayers;
+    private Pile<Card> discardPile;
+    private Pile<Card> drawStack;
+
+    public MexicanStandoffEvent(List<Player> listOfPlayers, Pile<Card> discardPile, Pile<Card> drawStack) {
+        this.listOfPlayers = listOfPlayers;
+        this.discardPile = discardPile;
+        this.drawStack = drawStack;
+    }
+
     public String getName() {
         return "mexican-standoff";
     }
 
     public void performEvent() {
+        for (Player player : this.listOfPlayers) {
+            for (int i = player.getHandSize() - 1; i >= 0; i--) {
+                this.discardPile.push(player.popCard(i));
+            }
+            for (int i = 0; i < 3; i++) {
+                if (!drawStack.empty()) {
+                    player.pushCardToHand(drawStack.pop());
+                }
+            }
+        }
     }
 
     public String getMessage() {
--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/MexicanStandoffEventTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/MexicanStandoffEventTest.java
@@ -1,16 +1,54 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.constant.Type;
+import ch.uzh.ifi.seal.soprafs20.constant.Value;
+import ch.uzh.ifi.seal.soprafs20.entity.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MexicanStandoffEventTest {
 
-    private Event mexicanStandoff = new MexicanStandoffEvent();
+    private List<Player> listOfPlayers = new ArrayList<>();
+    private Pile<Card> discardPile = new DiscardPile();
+    private Pile<Card> drawStack = new DrawStack();
+    private Player player1 = new Player();
+    private Event mexicanStandoff;
+
+    @BeforeEach
+    public void setup() {
+        player1.pushCardToHand(new Card(Color.RED, 7, 11));
+        player1.pushCardToHand(new Card(Color.MULTICOLOR, Type.SPECIAL, Value.FANTASTIC, false, 12));
+        player1.pushCardToHand(new Card(Color.BLUE, Type.SPECIAL, Value.SKIP, true, 13));
+        player1.pushCardToHand(new Card(Color.GREEN, Type.SPECIAL, Value.SECONDCHANCE, true, 14));
+        listOfPlayers.add(player1);
+
+        mexicanStandoff = new MexicanStandoffEvent(listOfPlayers, discardPile, drawStack);
+    }
 
     @Test
     public void getNameTest() {
         assertEquals("mexican-standoff", mexicanStandoff.getName());
+    }
+
+    @Test
+    public void performEventTest() {
+        Card c = new Card(Color.RED, 3, 1);
+
+        assertEquals(4, this.player1.getHandSize());
+        assertEquals(124, this.drawStack.size());
+
+        this.mexicanStandoff.performEvent();
+        assertEquals(3, this.player1.getHandSize());
+        assertEquals(4, this.discardPile.size());
+        assertEquals(121, this.drawStack.size());
     }
 
     @Test
