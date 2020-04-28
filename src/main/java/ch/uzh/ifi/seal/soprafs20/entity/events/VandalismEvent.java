dfec08eeee802b90dee08ee9479499d6433dbbff
JaanWilli <jan.willi@uzh.ch>
2020-04-28 11:50:28 +0200

    VandalismEvent: added event logic and test

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/VandalismEvent.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/VandalismEvent.java
@@ -1,11 +1,41 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.Pile;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+
+import java.util.List;
+
 public class VandalismEvent implements Event {
+
+    private final List<Player> listOfPlayers;
+    private Pile<Card> discardPile;
+
+    public VandalismEvent(List<Player> playerList, Pile<Card> discardPile) {
+        this.listOfPlayers = playerList;
+        this.discardPile = discardPile;
+    }
+
     public String getName() {
         return "vandalism";
     }
 
     public void performEvent() {
+
+        int index = 1;
+        Card relevant = this.discardPile.peekN(index);
+        while (relevant.getColor().equals(Color.BLACK) || relevant.getColor().equals(Color.MULTICOLOR)) {
+            relevant = this.discardPile.peekN(index++);
+        }
+
+        for (Player player : this.listOfPlayers) {
+            for (int i = player.getHandSize() - 1; i >= 0; i--) {
+                if (player.peekCard(i).getColor().equals(relevant.getColor())) {
+                    this.discardPile.push(player.popCard(i));
+                }
+            }
+        }
     }
 
     public String getMessage() {
--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/VandalismEventTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/VandalismEventTest.java
@@ -1,16 +1,51 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.constant.Type;
+import ch.uzh.ifi.seal.soprafs20.constant.Value;
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.DiscardPile;
+import ch.uzh.ifi.seal.soprafs20.entity.Pile;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VandalismEventTest {
 
-    private Event vandalism = new VandalismEvent();
+    private List<Player> listOfPlayers = new ArrayList<>();
+    private Pile<Card> discardPile = new DiscardPile();
+    private Player player1 = new Player();
+    private Event vandalism;
+
+    @BeforeEach
+    public void setup() {
+        player1.pushCardToHand(new Card(Color.RED, 7, 11));
+        player1.pushCardToHand(new Card(Color.MULTICOLOR, Type.SPECIAL, Value.FANTASTIC, false, 12));
+        player1.pushCardToHand(new Card(Color.RED, Type.SPECIAL, Value.SKIP, true, 13));
+        listOfPlayers.add(player1);
+
+        this.discardPile.push(new Card(Color.RED, 3, 1));
+        this.discardPile.push(new Card(Color.BLACK, 3, 2));
+
+        vandalism = new VandalismEvent(listOfPlayers, discardPile);
+    }
 
     @Test
     public void getNameTest() {
         assertEquals("vandalism", vandalism.getName());
+    }
+
+    @Test
+    public void performEventTest() {
+        assertEquals(3, this.player1.getHandSize());
+        this.vandalism.performEvent();
+        assertEquals(1, this.player1.getHandSize());
     }
 
     @Test
