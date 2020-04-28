02f1c62d79509886a5ebfe626a02d08b91fa39d0
JaanWilli <jan.willi@uzh.ch>
2020-04-28 11:50:42 +0200

    RecessionEvent: added event logic and test

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/RecessionEvent.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/RecessionEvent.java
@@ -1,11 +1,55 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.Pile;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import ch.uzh.ifi.seal.soprafs20.service.GameService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecessionEvent implements Event {
+
+    private String lobbyId;
+    private final List<Player> listOfPlayers;
+    private Player currentPlayer;
+    private Pile<Card> discardPile;
+    private int amount;
+
+    private final GameService gameService;
+
+    public RecessionEvent(String lobbyId, Player currentPlayer, List<Player> listOfPlayers, Pile<Card> discardPile, GameService gameService) {
+        this.lobbyId = lobbyId;
+        this.currentPlayer = currentPlayer;
+        this.listOfPlayers = listOfPlayers;
+        this.discardPile = discardPile;
+        this.gameService = gameService;
+        this.amount = 1;
+    }
+
     public String getName() {
         return "recession";
     }
 
     public void performEvent() {
+        Player p = this.currentPlayer;
+        List<Player> orderedListOfPlayers = new ArrayList<>();
+        orderedListOfPlayers.add(p);
+        for (int i = 0; i < this.listOfPlayers.size(); i++) {
+            p = nextPlayer(p);
+            orderedListOfPlayers.add(p);
+        }
+
+        for (Player player : orderedListOfPlayers) {
+            this.gameService.sendRecession(this.lobbyId, player, this.amount);
+            amount++;
+        }
+    }
+
+    private Player nextPlayer(Player prev) {
+        int playersIndex = this.listOfPlayers.indexOf(prev);
+        playersIndex = (playersIndex + 1) % this.listOfPlayers.size();
+        return this.listOfPlayers.get(playersIndex);
     }
 
     //TODO: make it dynamic
--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/RecessionEventTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/RecessionEventTest.java
@@ -1,16 +1,54 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.entity.Card;
+import ch.uzh.ifi.seal.soprafs20.entity.DiscardPile;
+import ch.uzh.ifi.seal.soprafs20.entity.Pile;
+import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import ch.uzh.ifi.seal.soprafs20.service.GameService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class RecessionEventTest {
 
-    private Event recession = new RecessionEvent();
+    private List<Player> listOfPlayers = new ArrayList<>();
+    private Pile<Card> discardPile = new DiscardPile();
+    private Player player1 = new Player();
+    private Player player2 = new Player();
+    private Event recession;
+
+    @Mock
+    private GameService gameService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        listOfPlayers.add(player1);
+        listOfPlayers.add(player2);
+
+        recession = new RecessionEvent("abc", player1, listOfPlayers, discardPile, gameService);
+    }
 
     @Test
     public void getNameTest() {
         assertEquals("recession", recession.getName());
+    }
+
+    @Test
+    public void performEventTest() {
+        Mockito.doNothing().when(gameService).sendRecession(Mockito.any(), Mockito.any(), Mockito.anyInt());
+
+        recession.performEvent();
+        Mockito.verify(gameService, Mockito.times(1)).sendRecession(Mockito.matches("abc"), Mockito.any(), Mockito.eq(1));
+        Mockito.verify(gameService, Mockito.times(1)).sendRecession(Mockito.matches("abc"), Mockito.any(), Mockito.eq(2));
     }
 
     @Test
