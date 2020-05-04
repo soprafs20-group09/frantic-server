c8486b173e32b6a68b469e8eba4df2fefc5299bd
JaanWilli <jan.willi@uzh.ch>
2020-05-04 17:59:17 +0200

    TheAllSeeingEyeEvent: implemented event

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/TheAllSeeingEyeEvent.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/TheAllSeeingEyeEvent.java
@@ -1,16 +1,39 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
 import ch.uzh.ifi.seal.soprafs20.entity.Chat;
+import ch.uzh.ifi.seal.soprafs20.entity.GameRound;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TheAllSeeingEyeEvent implements Event {
+
+    private final GameRound gameRound;
+    private final int seconds;
+
+    public TheAllSeeingEyeEvent(GameRound gameRound) {
+        this.gameRound = gameRound;
+        seconds = 30;
+    }
+
     public String getName() {
         return "the-all-seeing-eye";
     }
 
     public List<Chat> performEvent() {
+
+        this.gameRound.setShowCards(true);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                gameRound.setShowCards(false);
+            }
+        };
+        timer.schedule(timerTask, 1000 * seconds);
+
         List<Chat> chat = new ArrayList<>();
         chat.add(new Chat("event", "event:the-all-seeing-eye", this.getMessage()));
         return chat;
--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/TheAllSeeingEyeEventTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/TheAllSeeingEyeEventTest.java
@@ -1,12 +1,18 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
+import ch.uzh.ifi.seal.soprafs20.entity.GameRound;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TheAllSeeingEyeEventTest {
 
-    private Event theAllSeeingEye = new TheAllSeeingEyeEvent();
+    @Mock
+    private GameRound gameRound;
+
+    private Event theAllSeeingEye = new TheAllSeeingEyeEvent(gameRound);
 
     @Test
     public void getNameTest() {
