40901338c53ab2327fb79014471001e7a1337036
JaanWilli <jan.willi@uzh.ch>
2020-05-07 18:28:47 +0200

    GamblingManEventTest: added

--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/GamblingManEventTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/GamblingManEventTest.java
@@ -1,8 +1,9 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
-import ch.uzh.ifi.seal.soprafs20.entity.Game;
-import ch.uzh.ifi.seal.soprafs20.entity.GameRound;
-import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.constant.Type;
+import ch.uzh.ifi.seal.soprafs20.constant.Value;
+import ch.uzh.ifi.seal.soprafs20.entity.*;
 import ch.uzh.ifi.seal.soprafs20.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,6 +38,29 @@ public class GamblingManEventTest {
     public void getNameTest() {
         GamblingManEvent gamblingMan = new GamblingManEvent(this.gameRound);
         assertEquals("gambling-man", gamblingMan.getName());
+    }
+
+    @Test
+    public void performEventTest() {
+        Mockito.doNothing().when(gameService).sendGamblingMan(Mockito.any(), Mockito.any(), Mockito.any());
+        Mockito.doNothing().when(gameService).sendTimer(Mockito.any(), Mockito.anyInt());
+
+        Player player1 = new Player();
+        player1.pushCardToHand(new Card(Color.RED, 7, 11));
+        player1.pushCardToHand(new Card(Color.MULTICOLOR, Type.SPECIAL, Value.FANTASTIC, false, 12));
+        player1.pushCardToHand(new Card(Color.RED, Type.SPECIAL, Value.SKIP, true, 13));
+        this.listOfPlayers.add(player1);
+
+        DiscardPile pile = new DiscardPile();
+        pile.push(new Card(Color.RED, 3, 1));
+        pile.push(new Card(Color.BLACK, 3, 2));
+        Mockito.when(this.gameRound.getDiscardPile()).thenReturn(pile);
+        Mockito.when(this.gameRound.getCurrentPlayer()).thenReturn(player1);
+
+        Event gamblingMan = new GamblingManEvent(this.gameRound);
+        gamblingMan.performEvent();
+
+        Mockito.verify(gameService).sendGamblingMan(Mockito.any(), Mockito.any(), Mockito.any());
     }
 
     @Test
