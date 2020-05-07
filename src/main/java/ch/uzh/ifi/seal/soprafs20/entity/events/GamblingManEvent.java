98d13a077f4b4996235e4a75054a5b812ae66703
JaanWilli <jan.willi@uzh.ch>
2020-05-07 16:45:46 +0200

    GamblingManEvent: added event

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/GamblingManEvent.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/events/GamblingManEvent.java
@@ -1,7 +1,7 @@
 package ch.uzh.ifi.seal.soprafs20.entity.events;
 
-import ch.uzh.ifi.seal.soprafs20.entity.Chat;
-import ch.uzh.ifi.seal.soprafs20.entity.GameRound;
+import ch.uzh.ifi.seal.soprafs20.constant.Color;
+import ch.uzh.ifi.seal.soprafs20.entity.*;
 import ch.uzh.ifi.seal.soprafs20.service.GameService;
 
 import java.util.ArrayList;
@@ -11,10 +11,14 @@ public class GamblingManEvent implements Event {
 
     private final GameRound gameRound;
     private final GameService gameService;
+    private final List<Player> listOfPlayers;
+    private final Pile<Card> discardPile;
 
     public GamblingManEvent(GameRound gameRound) {
         this.gameRound = gameRound;
         this.gameService = gameRound.getGameService();
+        this.listOfPlayers = gameRound.getListOfPlayers();
+        this.discardPile = gameRound.getDiscardPile();
     }
 
     public String getName() {
@@ -22,7 +26,27 @@ public class GamblingManEvent implements Event {
     }
 
     public void performEvent() {
-
+        int index = 1;
+        Color relevant = this.discardPile.peekN(index).getColor();
+        while (relevant.equals(Color.BLACK) || relevant.equals(Color.MULTICOLOR)) {
+            relevant = this.discardPile.peekN(index++).getColor();
+        }
+        for (Player player : this.listOfPlayers) {
+            List<Integer> cards = new ArrayList<>();
+            for (int i = 0; i < player.getHandSize(); i++) {
+                if (player.peekCard(i).getColor().equals(relevant)) {
+                    cards.add(i);
+                }
+            }
+            if (!cards.isEmpty()) {
+                int[] playable = cards.stream().mapToInt(i -> i).toArray();
+                this.gameService.sendGamblingMan(this.gameRound.getLobbyId(), player, playable);
+            }
+            // no such color
+            else {
+                this.gameRound.drawCardFromStack(player, 2);
+            }
+        }
     }
 
     public String getMessage() {
