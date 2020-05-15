be1e3e33e4bf53f6ecd16be12d3aaf6b93192761
regloff <45421260+regloff@users.noreply.github.com>
2020-05-15 20:55:42 +0200

    GameRoundIntegrationTest: Made existing tests more readable & finished playCard tests

--- src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/GameRoundIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/entity/events/GameRoundIntegrationTest.java
@@ -79,26 +79,28 @@ public class GameRoundIntegrationTest {
 
         Mockito.verify(this.gameService, Mockito.never()).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService, Mockito.never()).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
+        assertEquals(2, player2.getHandSize());
         assertEquals(pileCard, testRound.getDiscardPile().peek());
         assertEquals(player1, testRound.getCurrentPlayer());
     }
 
     @Test
     public void playNumberCard_success() {
-        Card card = player1.peekCard(1);
+        Card cardToPlay = player1.peekCard(1);
         testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 10));
         testRound.startTurnTimer(30);
         testRound.playCard("id1", 1);
 
         Mockito.verify(this.gameService).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
-        assertEquals(card, testRound.getDiscardPile().peek());
+        assertEquals(2, player1.getHandSize());
+        assertEquals(cardToPlay, testRound.getDiscardPile().peek());
         assertEquals(player2, testRound.getCurrentPlayer());
     }
 
     @Test
     public void playBlackNumberCard_success_prepareEvent() {
-        Card card = player1.peekCard(0);
+        Card cardToPlay = player1.peekCard(0);
         testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 10));
         testRound.startTurnTimer(30);
         testRound.playCard("id1", 0);
@@ -106,11 +108,12 @@ public class GameRoundIntegrationTest {
         Mockito.verify(this.gameService).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
         Mockito.verify(this.gameService).sendPlayable("lobbyId", player1, new int[0], false, false);
-        Mockito.verify(this.gameService).sendGameState("lobbyId", card, this.testRound.getListOfPlayers(), false);
+        Mockito.verify(this.gameService).sendGameState("lobbyId", cardToPlay, this.testRound.getListOfPlayers(), false);
         Mockito.verify(this.gameService).sendEvent(Mockito.any(), Mockito.any());
         Mockito.verify(this.gameService).sendTimer("lobbyId", 11);
 
-        assertEquals(card, testRound.getDiscardPile().peek());
+        assertEquals(2, player1.getHandSize());
+        assertEquals(cardToPlay, testRound.getDiscardPile().peek());
         assertEquals(player1, testRound.getCurrentPlayer());
     }
 
@@ -123,6 +126,7 @@ public class GameRoundIntegrationTest {
 
         Mockito.verify(this.gameService, Mockito.never()).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService, Mockito.never()).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
+        assertEquals(3, player1.getHandSize());
         assertEquals(pileCard, testRound.getDiscardPile().peek());
         assertEquals(player1, testRound.getCurrentPlayer());
     }
@@ -136,15 +140,17 @@ public class GameRoundIntegrationTest {
         player1.pushCardToHand(new Card(Color.BLACK, Type.SPECIAL, Value.FUCKYOU, false, 14));
         player1.pushCardToHand(new Card(Color.BLUE, Type.NUMBER, Value.THREE, false, 15));
         player1.pushCardToHand(new Card(Color.BLUE, Type.NUMBER, Value.THREE, false, 16));
+        assertEquals(10, player1.getHandSize());
 
-        Card card = player1.peekCard(7);
+        Card fuckYou = player1.peekCard(7);
         testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 10));
         testRound.startTurnTimer(30);
         testRound.playCard("id1", 7);
 
         Mockito.verify(this.gameService).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
-        assertEquals(card, testRound.getDiscardPile().peek());
+        assertEquals(9, player1.getHandSize());
+        assertEquals(fuckYou, testRound.getDiscardPile().peek());
         assertEquals(player2, testRound.getCurrentPlayer());
     }
 
@@ -156,7 +162,6 @@ public class GameRoundIntegrationTest {
         player1.pushCardToHand(new Card(Color.BLUE, Type.NUMBER, Value.THREE, false, 13));
         player1.pushCardToHand(new Card(Color.BLACK, Type.SPECIAL, Value.FUCKYOU, false, 14));
 
-        Card card = player1.peekCard(7);
         Card pileCard = new Card(Color.YELLOW, Type.NUMBER, Value.EIGHT, false, 10);
         testRound.getDiscardPile().push(pileCard);
         testRound.startTurnTimer(30);
@@ -164,22 +169,168 @@ public class GameRoundIntegrationTest {
 
         Mockito.verify(this.gameService, Mockito.never()).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService, Mockito.never()).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
+        assertEquals(8, player1.getHandSize());
         assertEquals(pileCard, testRound.getDiscardPile().peek());
         assertEquals(player1, testRound.getCurrentPlayer());
     }
 
     @Test
     public void play2ChanceCard_success() {
-        player1.pushCardToHand(new Card(Color.BLUE, Type.SPECIAL, Value.SECONDCHANCE, false, 10));
-        Card card = player1.peekCard(3);
+        Card secondChance = new Card(Color.BLUE, Type.SPECIAL, Value.SECONDCHANCE, false, 10);
+        player1.pushCardToHand(secondChance);
         testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 11));
         testRound.startTurnTimer(30);
         testRound.playCard("id1", 3);
 
         Mockito.verify(this.gameService).sendHand("lobbyId", player1);
         Mockito.verify(this.gameService).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
-        Mockito.verify(this.gameService).sendPlayable("lobbyId", player1, player1.getPlayableCards(card), true, false);
-        assertEquals(card, testRound.getDiscardPile().peek());
+        Mockito.verify(this.gameService).sendPlayable("lobbyId", player1, player1.getPlayableCards(secondChance), true, false);
+        assertEquals(3, player1.getHandSize());
+        assertEquals(secondChance, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    @Test
+    public void playFantasticFourCard_success_45sec() {
+        Card fantasticFour = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.FANTASTICFOUR, true, 10);
+        player1.pushCardToHand(fantasticFour);
+        testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 11));
+        testRound.startTurnTimer(30);
+        testRound.playCard("id1", 3);
+
+        Mockito.verify(this.gameService).sendHand("lobbyId", player1);
+        Mockito.verify(this.gameService).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
+        Mockito.verify(this.gameService).sendTimer("lobbyId", 45);
+        Mockito.verify(this.gameService).sendActionResponse("lobbyId", player1, fantasticFour);
+        assertEquals(3, player1.getHandSize());
+        assertEquals(fantasticFour, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    @Test
+    public void playEqualityCard_success_30sec() {
+        Card equality = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.EQUALITY, false, 10);
+        player1.pushCardToHand(equality);
+        testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 11));
+        testRound.startTurnTimer(30);
+        testRound.playCard("id1", 3);
+
+        Mockito.verify(this.gameService).sendHand("lobbyId", player1);
+        Mockito.verify(this.gameService).sendChatMessage(Mockito.any(), (Chat) Mockito.any());
+        Mockito.verify(this.gameService).sendTimer("lobbyId", 30);
+        Mockito.verify(this.gameService).sendActionResponse("lobbyId", player1, equality);
+        assertEquals(3, player1.getHandSize());
+        assertEquals(equality, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    @Test
+    public void playNiceTry_niceTryOpportunity_success() {
+        player4.popCard();
+        player4.popCard();
+        player4.popCard();
+        assertEquals(0, player4.getHandSize());
+
+        Card niceTry = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.NICETRY, false, 10);
+        player3.pushCardToHand(niceTry);
+        testRound.getDiscardPile().push(new Card(Color.BLUE, Type.NUMBER, Value.EIGHT, false, 11));
+        testRound.startTurnTimer(30);
+        testRound.playCard("id3", 2);
+
+        Mockito.verify(this.gameService).sendPlayable("lobbyId", player3, new int[0], false, false);
+        Mockito.verify(this.gameService).sendHand("lobbyId", player3);
+        Mockito.verify(this.gameService, Mockito.times(2)).sendChatMessage(Mockito.any(), (Chat) Mockito.any()); //invocation for card played & drawn cards
+        Mockito.verify(this.gameService, Mockito.times(2)).sendHand("lobbyId", player4); //invocation for card played & drawn cards
+        Mockito.verify(this.gameService).sendActionResponse("lobbyId", player3, niceTry);
+        Mockito.verify(this.gameService).sendTimer("lobbyId", 30);
+
+        assertEquals(2, player3.getHandSize());
+        assertEquals(3, player4.getHandSize());
+        assertEquals(niceTry, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    @Test
+    public void playCounterattack_niceTryOpportunity_unsuccessful() {
+        player4.popCard();
+        player4.popCard();
+        player4.popCard();
+        assertEquals(0, player4.getHandSize());
+
+        Card counterattack = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.COUNTERATTACK, false, 10);
+        player3.pushCardToHand(counterattack);
+        Card pileCard = new Card(Color.YELLOW, Type.NUMBER, Value.EIGHT, false, 11);
+        testRound.getDiscardPile().push(pileCard);
+        testRound.startTurnTimer(30);
+        testRound.playCard("id3", 2);
+
+        Mockito.verify(this.gameService, Mockito.never()).sendPlayable("lobbyId", player3, new int[0], false, false);
+        Mockito.verify(this.gameService, Mockito.never()).sendHand("lobbyId", player3);
+
+        assertEquals(3, player3.getHandSize());
+        assertEquals(0, player4.getHandSize());
+        assertEquals(pileCard, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    @Test
+    public void playCounterAttack_counterAttackOpportunity_success() {
+        testRound.startTurnTimer(30);
+        testRound.storeSkipAction("id1", "player3");
+
+        Card counterattack = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.COUNTERATTACK, false, 10);
+        player3.pushCardToHand(counterattack);
+        Card actionCard = new Card(Color.BLUE, Type.SPECIAL, Value.SKIP, true, 11);
+        testRound.getDiscardPile().push(actionCard);
+        testRound.playCard("id3", 2);
+
+        Mockito.verify(this.gameService, Mockito.times(2)).sendPlayable("lobbyId", player3, new int[0], false, false); //once in prepareCounterattack & playCounterattack
+        Mockito.verify(this.gameService).sendHand("lobbyId", player3);
+        Mockito.verify(this.gameService, Mockito.times(2)).sendChatMessage(Mockito.any(), (Chat) Mockito.any()); //once in prepareCounterattack & playCounterattack
+        Mockito.verify(this.gameService).sendGameState("lobbyId", counterattack, this.testRound.getListOfPlayers(), false);
+        Mockito.verify(this.gameService).sendAttackTurn("lobbyId", "player3");
+        Mockito.verify(this.gameService).sendActionResponse("lobbyId", player3, actionCard);
+        Mockito.verify(this.gameService).sendTimer("lobbyId", 30);
+
+        assertEquals(2, player3.getHandSize());
+        assertEquals(counterattack, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    @Test
+    public void playCounterAttack_counterAttackOpportunity_notTarget() {
+        testRound.startTurnTimer(30);
+        testRound.storeSkipAction("id1", "player2");
+
+        Card counterattack = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.COUNTERATTACK, false, 10);
+        player3.pushCardToHand(counterattack);
+        Card actionCard = new Card(Color.BLUE, Type.SPECIAL, Value.SKIP, true, 11);
+        testRound.getDiscardPile().push(actionCard);
+        testRound.playCard("id3", 2);
+
+        Mockito.verify(this.gameService, Mockito.never()).sendHand("lobbyId", player3);
+
+        assertEquals(3, player3.getHandSize());
+        assertEquals(actionCard, testRound.getDiscardPile().peek());
+        assertEquals(player1, testRound.getCurrentPlayer());
+    }
+
+    //if someone tries to play a nice try to block an attack
+    @Test
+    public void playNiceTry_counterAttackOpportunity_unsuccessful() {
+        testRound.startTurnTimer(30);
+        testRound.storeSkipAction("id1", "player3");
+
+        Card niceTry = new Card(Color.MULTICOLOR, Type.SPECIAL, Value.NICETRY, false, 10);
+        player3.pushCardToHand(niceTry);
+        Card actionCard = new Card(Color.BLUE, Type.SPECIAL, Value.SKIP, true, 11);
+        testRound.getDiscardPile().push(actionCard);
+        testRound.playCard("id3", 2);
+
+        Mockito.verify(this.gameService, Mockito.never()).sendHand("lobbyId", player3);
+
+        assertEquals(3, player3.getHandSize());
+        assertEquals(actionCard, testRound.getDiscardPile().peek());
         assertEquals(player1, testRound.getCurrentPlayer());
     }
 }
