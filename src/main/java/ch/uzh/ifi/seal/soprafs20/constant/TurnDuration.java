3ab6b0cd3ef030448fa85557fc4cf8ee4ad91e37
Davfon <davide.fontanella@uzh.ch>
2021-02-17 20:50:26 +0100

    TurnDuration: assign second values to enum

--- src/main/java/ch/uzh/ifi/seal/soprafs20/constant/TurnDuration.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/constant/TurnDuration.java
@@ -1,5 +1,17 @@
 package ch.uzh.ifi.seal.soprafs20.constant;
 
 public enum TurnDuration {
-    NORMAL, LONG, INFINITE
+    NORMAL(30),
+    LONG(60),
+    INFINITE(0);
+
+    private final int value;
+
+    TurnDuration (int i) {
+        this.value = i;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
